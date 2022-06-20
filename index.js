2162a24949c3c730dd2e2d66c33219b5df595732
Brian <brianekonz@gmail.com>
2022-06-20 18:31:32 -0300

    cambios

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ const express = require ('express')
 const app = express ()
 const puerto = 8080
 
+app.use((req, res, next) => {
+    visitas++
+})
+
 app.get('/', (req, res) => {
-    res.send('hola soy Brian')
-}) 
-
-app.get('/User', (req, res) => {
-    res.send('hola soy usuario')
-}) 
-
-app.listen(puerto, () => {
-    console.log(`el servidor esta escuchando el puerto: ${puerto}`)
-    console.log(`el servidor esta escuchando el puerto: ${puerto}`)
-    console.log(`el servidor esta escuchando el puerto: ${puerto}`)
     
+})
+
+app.get('/productos', (req, res) => {
+    
+})
+
+app.get('/productosRandom', (req, res) => {
+    
+})
+
+app.listen(puerto, (error) => {
+    if (!error){
+        console.log(`el servidor esta escuchando el puerto: ${puerto}`) 
+    } else
+    console.log(`Hubo un error al iniciar el server: ${puerto}`) 
 })
