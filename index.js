a130343d4f838faf2fff70423be36e6a46f5e09c
Brian <brianekonz@gmail.com>
2022-06-20 17:43:00 -0300

    nuevo repo

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require ('express')
+const app = express ()
+const puerto = 8080
+
+app.get('/', (req, res) => {
+    res.send('hola soy Brian')
+}) 
+
+app.get('/User', (req, res) => {
+    res.send('hola soy usuario')
+}) 
+
+app.listen(puerto, () => {
+    console.log(`el servidor esta escuchando el puerto: ${puerto}`)
+})
